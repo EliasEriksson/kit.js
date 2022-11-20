02d2b54b70233885d8cee315705080fd6fe206b9
eliaseriksson <eliaseriksson95@gmail.com>
2022-11-20 19:39:04 +0100

    reworked Iterable and added tests

--- Iterable/index.spec.ts
+++ Iterable/index.spec.ts
@@ -1,11 +1,90 @@
 import { structures } from "../index"
 
 describe("Iterable", () => {
+	it("range", () => {
+		expect(Array.from(structures.Iterable.range(0, 0, 0))).toEqual([0])
+		expect(Array.from(structures.Iterable.range(0, 1, 0))).toEqual([0, 1])
+		expect(Array.from(structures.Iterable.range(0, 10))).toEqual(Array.from({ length: 10 }, (_, i) => i))
+		expect(Array.from(structures.Iterable.range(0, -10, -1))).toEqual(Array.from({ length: 10 }, (_, i) => 0 - i))
+		expect(Array.from(structures.Iterable.range(0, 10, 2))).toEqual(
+			Array.from({ length: 10 }, (_, i) => i).filter(i => i % 2 == 0)
+		)
+		expect(Array.from(structures.Iterable.range(2, 5, 1))).toEqual([2, 3, 4])
+		expect(Array.from(structures.Iterable.range(2, 5, 2))).toEqual([2, 4])
+	})
+	it("array", () => {
+		expect(structures.Iterable.range(0, 10, 0).array()).toEqual([0, 10])
+	})
+	it("set", () => {
+		expect(structures.Iterable.range(0, 10, 0).set()).toEqual(new Set([0, 10]))
+	})
 	it("map", () => {
-		expect(Array.from(structures.Iterable.map([1, 2, 3], v => v))).toEqual([1, 2, 3])
+		expect(structures.Iterable.map([1, 2, 3], v => v * 2).array()).toEqual([2, 4, 6])
+		expect(structures.Iterable.map([1, 2, 3], v => v + 1).array()).toEqual([2, 3, 4])
+	})
+	it("mutability", () => {
+		const data = [1, 2, 3]
+		let iterable = structures.Iterable.map(data, n => n + 1)
+		expect(Array.from(iterable).length).toEqual(3)
+		expect(Array.from(iterable).length).not.toEqual(3)
+		iterable = structures.Iterable.range(0, 10)
+		expect(Array.from(iterable).length).toEqual(10)
+		expect(Array.from(iterable).length).not.toEqual(10)
+	})
+	it("chaining", () => {
+		expect(
+			structures.Iterable.range(0, 5)
+				.map(v => v + 1)
+				.map(v => v * 2)
+				.array()
+		).toEqual([2, 4, 6, 8, 10])
 	})
 	it("reduce", () => {
-		expect(structures.Iterable.reduce([1, 2, 3], (a, c) => a + c, 0)).toEqual(6)
-		expect(structures.Iterable.reduce([], v => v, 0)).toEqual(0)
+		expect(structures.Iterable.range(0, 10).reduce((sum, value) => sum + value, 0)).toEqual(45)
+		expect(structures.Iterable.reduce("asd", (aggregate, current) => current + aggregate, "")).toEqual("dsa")
+	})
+	it("filter", () => {
+		expect(
+			structures.Iterable.range(0, 10)
+				.filter(v => v % 2 == 0)
+				.reduce((sum, value) => sum + value, 0)
+		).toEqual(20)
+		expect(structures.Iterable.filter<string>("asd", v => v != "s").array()).toEqual(["a", "d"])
+	})
+	it("concatenate", () => {
+		expect(structures.Iterable.concatenate([1], [2]).concatenate([3]).array()).toEqual([1, 2, 3])
+	})
+	it("push", () => {
+		expect(structures.Iterable.push([1], 2).push(3, 4).array()).toEqual([1, 2, 3, 4])
+	})
+	it("includes", () => {
+		expect(structures.Iterable.range(0, 10).includes(5)).toEqual(true)
+		expect(structures.Iterable.includes([0, 1, 2, 3], 3)).toEqual(true)
+	})
+	it("every", () => {
+		expect(structures.Iterable.range(0, 2).every(v => v >= 0)).toEqual(true)
+		expect(structures.Iterable.range(-2, 2).every(v => v > 0)).toEqual(false)
+		expect(structures.Iterable.every([0, 1, 2], v => v >= 0)).toEqual(true)
+	})
+	it("some", () => {
+		expect(structures.Iterable.range(0, 2).some(v => v == 1)).toEqual(true)
+		expect(structures.Iterable.range(0, 2).some(v => v == 2)).toEqual(false)
+		expect(structures.Iterable.some([0, 1, 2], v => v == 2)).toEqual(true)
+	})
+	it("find", () => {
+		const people = structures.Iterable.range(0, 10)
+			.map(i => ({
+				age: 20 + i,
+			}))
+			.array()
+		expect(people.find(person => person.age == 25)).toEqual({ age: 25 })
+		expect(structures.Iterable.find(people, person => person.age == 25)).toEqual({ age: 25 })
+	})
+	it("unique", () => {
+		expect(structures.Iterable.unique([1, 1, 2, 3]).array()).toEqual([1, 2, 3])
+	})
+	it("fork", () => {
+		const [a, b] = structures.Iterable.range(0, 4).fork()
+		expect(a.array()).toEqual(b.array())
 	})
 })
--- Iterable/index.ts
+++ Iterable/index.ts
@@ -1,52 +1,166 @@
 export namespace Iterable_ {
-	export function* map<I, O>(iterable: Iterable<I>, mapping: (value: I) => O): Iterable<O> {
-		for (const value of iterable)
-			yield mapping(value)
+	class Iterable<I> implements globalThis.Iterable<I> {
+		constructor(private readonly iterable: globalThis.Iterable<I>) {}
+		[Symbol.iterator](): Iterator<I, any, undefined> {
+			return this.iterable[Symbol.iterator]()
+		}
+		forEach(callback: (value: I) => void): void {
+			Iterable_.forEach(this, callback)
+		}
+		map<O>(mapping: (value: I) => O): Iterable<O> {
+			function* generator(iterable: Iterable<I>, mapping: (value: I) => O): globalThis.Iterable<O> {
+				for (const value of iterable)
+					yield mapping(value)
+			}
+			return new Iterable(generator(this, mapping))
+		}
+		reduce<O>(reducer: (aggregate: O, value: I) => O, initial: O): O {
+			return Iterable_.reduce(this, reducer, initial)
+		}
+		filter(filter: (value: I) => boolean): Iterable<I> {
+			function* generator(iterable: Iterable<I>, filter: (value: I) => boolean): globalThis.Iterable<I> {
+				for (const value of iterable)
+					filter(value) && (yield value)
+			}
+			return new Iterable(generator(this, filter))
+		}
+		concatenate(iterable: globalThis.Iterable<I>): Iterable<I> {
+			function* generator(a: globalThis.Iterable<I>, b: globalThis.Iterable<I>): globalThis.Iterable<I> {
+				for (const value of a)
+					yield value
+				for (const value of b)
+					yield value
+			}
+			return new Iterable(generator(this, iterable))
+		}
+		push(...values: I[]): Iterable<I> {
+			function* generator(iterable: globalThis.Iterable<I>, values: I[]): globalThis.Iterable<I> {
+				for (const value of iterable)
+					yield value
+				for (const value of values)
+					yield value
+			}
+			return new Iterable(generator(this, values))
+		}
+		every(test: (value: I) => boolean): boolean {
+			return Iterable_.every(this, test)
+		}
+		some(test: (value: I) => boolean): boolean {
+			return Iterable_.some(this, test)
+		}
+		find(finder: (value: I) => boolean): I | undefined {
+			return Iterable_.find(this, finder)
+		}
+		includes(needle: I): boolean {
+			return Iterable_.includes(this, needle)
+		}
+		array(): I[] {
+			return Array.from(this)
+		}
+		set(): Set<I> {
+			const set = new Set<I>()
+			for (const value of this)
+				set.add(value)
+			return set
+		}
+		unique(): Iterable<I> {
+			function* generator(iterable: globalThis.Iterable<I>): globalThis.Iterable<I> {
+				const history = new Set<I>()
+				for (const value of iterable)
+					if (!history.has(value))
+						history.add(value), yield value
+			}
+			return new Iterable(generator(this))
+		}
+		fork(): [Iterable<I>, Iterable<I>] {
+			const historyA: I[] = []
+			const historyB: I[] = []
+			function* generator(iterable: globalThis.Iterable<I>, self: I[], other: I[]): globalThis.Iterable<I> {
+				for (let next = self[0]; self.length; next = self[0])
+					self.shift(), yield next
+				for (const value of iterable) {
+					other.push(value)
+					yield value
+					for (let next = self[0]; self.length; next = self[0])
+						self.shift(), yield next
+				}
+			}
+			return [new Iterable(generator(this, historyA, historyB)), new Iterable(generator(this, historyB, historyA))]
+		}
+		static range(start: number, end: number, step = 1): Iterable<number> {
+			function* generator(start: number, end: number, step: number): globalThis.Iterable<number> {
+				if (start == end)
+					yield start
+				else if (step == 0) {
+					yield start
+					yield end
+				} else if (start < end)
+					for (let value = start; value < end; value += step)
+						yield value
+				else
+					for (let value = start; value > end; value += step)
+						yield value
+			}
+			return new Iterable(generator(start, end, step))
+		}
 	}
-	export function reduce<I, O>(iterable: Iterable<I>, reducer: (aggregate: O, value: I) => O, initial: O): O {
+	export function forEach<I>(iterable: globalThis.Iterable<I>, callback: (value: I) => void): void {
+		for (const value of iterable)
+			callback(value)
+	}
+	export function map<I, O>(iterable: globalThis.Iterable<I>, mapping: (value: I) => O): Iterable<O> {
+		return new Iterable(iterable).map(mapping)
+	}
+	export function reduce<I, O>(
+		iterable: globalThis.Iterable<I>,
+		reducer: (aggregate: O, value: I) => O,
+		initial: O
+	): O {
 		let aggregate = initial
 		for (const value of iterable)
 			aggregate = reducer(aggregate, value)
 		return aggregate
 	}
-	export function* filter<I>(iterable: Iterable<I>, filter: (value: I) => boolean): Iterable<I> {
-		for (const value of iterable)
-			filter(value) && (yield value)
+	export function filter<I>(iterable: globalThis.Iterable<I>, filter: (value: I) => boolean): Iterable<I> {
+		return new Iterable(iterable).filter(filter)
 	}
-	export function every<I>(iterable: Iterable<I>, test: (value: I) => boolean): boolean {
+	export function every<I>(iterable: globalThis.Iterable<I>, test: (value: I) => boolean): boolean {
 		for (const value of iterable)
 			if (!test(value))
 				return false
 		return true
 	}
-	export function some<I>(iterable: Iterable<I>, test: (value: I) => boolean): boolean {
+	export function some<I>(iterable: globalThis.Iterable<I>, test: (value: I) => boolean): boolean {
 		for (const value of iterable)
 			if (test(value))
 				return true
 		return false
 	}
-	export function find<I>(iterable: Iterable<I>, search: (value: I) => boolean): I | undefined {
+	export function find<I>(iterable: globalThis.Iterable<I>, finder: (value: I) => boolean): I | undefined {
 		for (const value of iterable)
-			if (search(value))
+			if (finder(value))
 				return value
 		return undefined
 	}
-	export function includes<I>(iterable: Iterable<I>, needle: I): boolean {
+	export function includes<I>(iterable: globalThis.Iterable<I>, needle: I): boolean {
 		for (const value of iterable)
 			if (value == needle)
 				return true
 		return false
 	}
-	export function* concatenate<I>(a: Iterable<I>, b: Iterable<I>): Iterable<I> {
-		for (const value of a)
-			yield value
-		for (const value of b)
-			yield value
+	export function concatenate<I>(a: globalThis.Iterable<I>, b: globalThis.Iterable<I>): Iterable<I> {
+		return new Iterable(a).concatenate(b)
 	}
-	export function* push<I>(iterable: Iterable<I>, ...values: I[]): Iterable<I> {
-		for (const value of iterable)
-			yield value
-		for (const value of values)
-			yield value
+	export function push<I>(iterable: globalThis.Iterable<I>, ...values: I[]): Iterable<I> {
+		return new Iterable(iterable).push(...values)
+	}
+	export function range(start: number, end: number, step = 1): Iterable<number> {
+		return Iterable.range(start, end, step)
+	}
+	export function unique<I>(iterable: globalThis.Iterable<I>): Iterable<I> {
+		return new Iterable(iterable).unique()
+	}
+	export function fork<I>(iterable: globalThis.Iterable<I>): [Iterable<I>, Iterable<I>] {
+		return new Iterable(iterable).fork()
 	}
 }
